3237d2655f80b06ff03968d02f0bdf7f0b1a40d5
oluwadareseyi <oluwadareseyii@gmail.com>
2021-03-19 16:36:00 +0100

    finish basic server

--- server.js
+++ server.js
@@ -1,86 +1,79 @@
 const request = require("request");
-const fs = require("fs");
 const express = require("express");
-const fileUpload = require("express-fileupload");
 const bodyParser = require("body-parser");
 const app = express();
 var cors = require("cors");
 
 require("dotenv/config");
-const postUrl =
-  "https://api.hubapi.com/filemanager/api/v3/files/upload?hapikey=" +
-  process.env.API_KEY;
+const baseUrl = "https://nigerialogos.com";
 
 app.use(bodyParser.json());
 
 app.use(cors());
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(
-  fileUpload({
-    useTempFiles: true,
-    safeFileNames: true,
-    preserveExtension: true,
-  })
-);
+
 app.get("/api/", (req, res) => {
-  res.json({ message: "BRIX-FIGMA-PLUGIN V1" });
+  res.json({ message: "Nigerian logos plugin" });
 });
 
-app.post("/api/upload", (req, res) => {
-  const filePath = req.files.file.tempFilePath;
-
-  const fileOptions = {
-    access: "PUBLIC_INDEXABLE",
-    ttl: "P3M",
-    overwrite: false,
-    duplicateValidationStrategy: "NONE",
-    duplicateValidationScope: "ENTIRE_PORTAL",
-  };
-
-  const formData = {
-    file: fs.createReadStream(filePath),
-    options: JSON.stringify(fileOptions),
-    folderPath: "docs",
-  };
-
-  return request.post(
-    {
-      url: postUrl,
-      formData: formData,
-    },
-    function optionalCallback(err, httpResponse, body) {
-      if (err) throw new Error(err);
-      fs.unlinkSync(filePath);
-      res.json(httpResponse);
-    }
-  );
-});
-
-app.get("/api/get-files", (req, res) => {
+app.get("/api/logos", async (req, res) => {
   return request.get(
     {
-      url: ` https://api.hubapi.com/filemanager/api/v2/files?hapikey=${process.env.API_KEY}`,
+      url: `${baseUrl}/logos.json`,
     },
     function optionalCallback(err, httpResponse, body) {
-      res.json(httpResponse.body);
+      if (httpResponse) {
+        res.statusCode = 200;
+        res.json(JSON.parse(httpResponse.body));
+      } else {
+        res.statusCode = 500;
+        res.json({ message: "a network error occured" });
+      }
     }
   );
 });
 
-app.post("/api/get-file", (req, res) => {
-  const image = req.body.image;
-
+app.get("/api/logos/:company", async (req, res) => {
+  const { company } = req.params;
   return request.get(
     {
-      url: image,
+      url: `${baseUrl}/logos/${company}/${company}.svg`,
     },
     function optionalCallback(err, httpResponse, body) {
-      res.json(httpResponse.body);
+      if (httpResponse) {
+        res.statusCode = 200;
+        res.json(httpResponse.body);
+      } else {
+        res.statusCode = 500;
+        res.json({ message: "a network error occured" });
+      }
     }
   );
 });
+
+// app.get("/api/get-files", (req, res) => {
+//   return request.get(
+//     {
+//       url: ` https://api.hubapi.com/filemanager/api/v2/files?hapikey=${process.env.API_KEY}`,
+//     },
+//     function optionalCallback(err, httpResponse, body) {
+//       res.json(httpResponse.body);
+//     }
+//   );
+// });
+
+// app.post("/api/get-file", (req, res) => {
+//   const image = req.body.image;
+
+//   return request.get(
+//     {
+//       url: image,
+//     },
+//     function optionalCallback(err, httpResponse, body) {
+//       res.json(httpResponse.body);
+//     }
+//   );
+// });
 
 app.listen(process.env.PORT || 5000);
-
-
